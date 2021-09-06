c4dfbcfc783249ea0c7e0976f142474a0882756b
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2021-09-05 22:46:40 -0500

    Express setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// cors policy
+app.use(cors({
+  origin: "*",
+}));
+
+// Server running and listening to port
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// 
+app.get('/', async (req, res) => {
+  try {
+    res.send({response: 'here it is'});
+  }
+  catch (e) {
+    console.log("Error fetching data for user: " + e.message);
+  };
+});
